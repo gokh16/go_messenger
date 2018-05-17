27c9425b483a358b25cbc06efdfdfc3f4a392851
Denys Terentiev <denys.terentiev@chisw.com>
2018-05-17 14:30:28 +0300

    some fixes

--- server/tcp/server.go
+++ server/tcp/server.go
@@ -1,56 +1,114 @@
 package main
 
 import (
-	"net"
-	"log"
 	"bufio"
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"io"
+	"container/list"
 )
 
-func main()  {
-	ln, err := net.Listen("tcp", "127.0.0.1:8081")
-	if err != nil {
-		log.Println(err.Error())
-	}
-	aconns := make(map[net.Conn]int)
-	conns := make(chan net.Conn)
-	dconns := make(chan net.Conn)
-	msgs := make(chan string)
-	i := 0
+var clients	*list.List
 
-	go func() {
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
+
+func SendMessageTo(socket net.Conn)  {
+	for {
+		buffer, err := bufio.NewReader(socket).ReadString('\n')
+		if err != nil {
+			log.Printf("Client disconnected")
+			socket.Close()
+			return
+		}
+		for i := clients.Front(); i != nil; i = i.Next() {
+			bufio.NewWriter(i.Value.(net.Conn)).WriteString(buffer)
+		}
+	}
+}
+
+func main() {
+
+	users := make(map[net.Conn]string) // Map of active connections
+	newConnection := make(chan net.Conn) // Handle new connection
+	addedUser := make(chan net.Conn)   // Add new connection
+	deadUser := make(chan net.Conn)    // Users that have left chat
+	messages := make(chan string)      // channel that recieves messages from all users
+
+	server, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	go func() { // Launch goroutine that will accept connections
 		for {
-			conn, err := ln.Accept()
+			conn, err := server.Accept()
 			if err != nil {
-				log.Println(err.Error())
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			conns <- conn
+			newConnection <- conn // Send to handle new user
+
 		}
 	}()
 
-	for {
+	for { // Run forever
+
 		select {
-		case conn := <-conns:
-			aconns[conn] = i
-			i++
-			go func(conn net.Conn, i int) {
-					rd := bufio.NewReader(conn)
-					for {
-						m, err := rd.ReadString('\n')
-						if err != nil {
-							break
-						}
-						msgs <- fmt.Sprintf("Client %v: %v", i, m)
+		case conn := <-newConnection:
+
+			go func(conn net.Conn) { // Ask user name
+				reader := bufio.NewReader(conn)
+				io.WriteString(conn, "Enter name: ")
+				userName, _ := reader.ReadString('\n')
+				userName = strings.Trim(userName, "\r\n")
+				log.Printf("Accepted new user : %s", userName)
+				messages <- fmt.Sprintf("Accepted user : [%s]\n\n", userName)
+				users[conn] = userName // Add connection
+				addedUser <- conn // Add user to pool
+			}(conn)
+
+		case conn := <-addedUser: // Launch a new goroutine for the newly added user
+
+			go func(conn net.Conn, userName string) {
+				reader := bufio.NewReader(conn)
+				for { // Run forever and handle this user's messages
+					newMessage, err := reader.ReadString('\n')
+					newMessage = strings.Trim(newMessage, "\r\n")
+					if err != nil {
+						break
 					}
-					dconns <- conn
-			}(conn, i)
-		case msg := <-msgs:
-			for conn := range aconns {
-				conn.Write([]byte(msg))
+					// Send to messages channel therefore ring every user
+					messages <- fmt.Sprintf(">%s: %s \a\n\n", userName, newMessage)
+				}
+
+				deadUser <- conn // If error occurs, connection has been terminated
+				messages <- fmt.Sprintf("%s disconnected\n\n", userName)
+			}(conn, users[conn])
+
+		case message := <-messages: // If message recieved from any user
+
+			for conn, _ := range users { // Send to all users
+				go func(conn net.Conn, message string) { // Write to all user connections
+					_, err := io.WriteString(conn, message)
+					if err != nil {
+						deadUser <- conn
+					}
+				}(conn, message)
+				log.Printf("New message: %s", message)
+				log.Printf("Sent to %d users", len(users))
 			}
-		case dconn := <-dconns:
-			log.Printf("Client %v is gone avay\n", aconns[dconn])
-			delete(aconns, dconn)
+
+		case conn := <-deadUser: // Handle dead users
+			log.Printf("Client disconnected")
+			delete(users, conn)
 		}
 	}
 }

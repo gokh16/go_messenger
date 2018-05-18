d96ac4b4cd28501ca271dce378134fac6c8b69c9
Misha Tkachenko <isfortis@gmail.com>
2018-05-18 23:59:57 +0300

    combine with the structure of the project

---
+++ desktop/desktop-client/event_handlers.go
@@ -0,0 +1,23 @@
+package desktop_client
+//
+//type mimetype struct {
+//	text string
+//
+//	/*		Other types
+//	*		.
+//	*		.
+//	*		.
+//	*/
+//}
+//
+//func SendMessageTo(msg string, msgType mimetype, group string, fromUser string){
+//
+//}
+//
+//func GetMessages(count int, group_name string){
+//
+//}
+//
+//func CreateGroup(group_name string, owner string, type string, members []string ){
+//
+//}

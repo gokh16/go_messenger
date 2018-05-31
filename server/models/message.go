fa932bb6374ebaf66ad3befc6de090224f950878
Максим Крыжановский <just_my_mail@coolsite.net>
2018-05-31 23:02:15 +0300

    cleaned dev and connected handlers

--- server/models/message.go
+++ server/models/message.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models
 
 import (
@@ -17,23 +16,3 @@ type Message struct {
 	MessageRecipientID   uint   `json:"message_recepient_id"`
 	MessageContentTypeID uint   `json:"message_content_type_id"`
 }
-=======
-package models
-
-import (
-	"github.com/jinzhu/gorm"
-)
-
-type Message struct {
-	gorm.Model
-
-	User               User
-	Group              Group
-	MessageContentType MessageContentType
-
-	Content              string `json:"message_content"`
-	MessageSenderID      uint   `json:"message_sender_id"`
-	MessageRecipientID   uint   `json:"message_recepient_id"`
-	MessageContentTypeID uint   `json:"message_content_type_id"`
-}
->>>>>>> 3661ec18fda6f6db02155e9be22dd834f0e1cd48
--- server/models/user_relation.go
+++ server/models/user_relation.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models
 
 import (
@@ -14,20 +13,3 @@ type UserRelation struct {
 	RelatedUser    uint `sql:"type:int REFERENCES users(id)"`
 	RelationTypeID uint
 }
-=======
-package models
-
-import (
-	"github.com/jinzhu/gorm"
-)
-
-type UserRelation struct {
-	gorm.Model
-
-	RelationType RelationType
-
-	RelatingUser   int `sql:"type:int REFERENCES users(id)"`
-	RelatedUser    int `sql:"type:int REFERENCES users(id)"`
-	RelationTypeID int
-}
->>>>>>> 3661ec18fda6f6db02155e9be22dd834f0e1cd48

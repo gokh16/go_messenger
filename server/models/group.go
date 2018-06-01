a04fdb4a2ae51ca02a4bdb9d8236e9d973686978
Aleksjj <komarov.oleksii@gmail.com>
2018-06-01 14:53:09 +0300

    resolved conflicts

--- server/models/group.go
+++ server/models/group.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models
 
 import (
@@ -15,21 +14,3 @@ type Group struct {
 	GroupOwnerID uint   `json:"group_owner_id"`
 	GroupTypeID  uint   `json:"group_type_id"`
 }
-=======
-package models
-
-import (
-	"github.com/jinzhu/gorm"
-)
-
-type Group struct {
-	gorm.Model
-
-	User      User
-	GroupType GroupType
-
-	GroupName    string `json:"group_name"`
-	GroupOwnerID uint   `json:"group_owner_id"`
-	GroupTypeID  uint   `json:"group_type_id"`
-}
->>>>>>> 3661ec18fda6f6db02155e9be22dd834f0e1cd48

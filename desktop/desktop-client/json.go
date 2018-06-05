4650e7d493f02652fe02ea5dfda3aaa8fd66075b
Misha Tkachenko <isfortis@gmail.com>
2018-06-05 16:57:35 +0300

    use of structures

--- desktop/desktop-client/json.go
+++ desktop/desktop-client/json.go
@@ -8,20 +8,30 @@ import (
 )
 
 type Message struct {
-	UserName    string
-	GroupName   string
-	ContentType string
-	Content     string
-	Login       string
-	Password    string
-	Email       string
-	Status      bool
-	UserIcon    string
-	Action      string
+	UserName     string
+	RelatingUser string
+	RelatedUser  string
+	RelationType uint
+	GroupName    string
+	GroupType    uint
+	GroupOwner   string
+	GroupMember  []string
+	ContentType  string
+	Content      string
+	LastMessage  string
+	Login        string
+	Password     string
+	Email        string
+	Status       bool
+	UserIcon     string
+	Action       string
 }
 
-func JSONencode(user string, groupName string, contentType string, content string, login string, password string, email string, status bool, userIcon string, action string) string {
-	incomingData := Message{user, groupName, contentType, content, login, password, email, status, userIcon, action}
+func JSONencode(user string, relatingUser string, relatedUser string, relationType uint, groupName string, groupType uint,
+	groupOwner string, groupMember []string, contentType string, content string, lastMessage string, login string, password string,
+	email string, status bool, userIcon string, action string) string {
+	incomingData := Message{user, relatingUser,relatedUser,relationType,groupName, groupType, groupOwner,
+	groupMember, contentType, content, lastMessage, login, password, email, status, userIcon, action}
 	outcomingData, err := json.Marshal(incomingData)
 	if err != nil {
 		log.Fatal(err)

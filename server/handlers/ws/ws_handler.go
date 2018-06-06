fbb542cc0b24ef5cf54363694579a4d4b0392945
Aleksjj <komarov.oleksii@gmail.com>
2018-06-06 16:53:36 +0300

    Changed ws server, message handling

---
+++ server/handlers/ws/ws_handler.go
@@ -0,0 +1,77 @@
+package ws
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gokh16/go_messenger/server/userConnections"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type WSHandler struct {
+	Connection *userConnections.Connections
+}
+
+func (c *WSHandler) NewWSHandler(conns *userConnections.Connections) {
+	ws := WSHandler{conns}
+	Handler(ws)
+}
+
+func Handler(str WSHandler) {
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("Cannot upgrade")
+		}
+		go ReadMessage(conn, str)
+	})
+	log.Println("HTTP server started on :12345")
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ReadMessage(conn *websocket.Conn, str WSHandler) {
+	for {
+		_, data, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Cannot read message")
+		}
+		GetJSON([]byte(data), conn, str)
+	}
+}
+
+func GetJSON(bytes []byte, conn *websocket.Conn, str WSHandler) chan *userConnections.Message {
+	message := userConnections.Message{}
+	err := json.Unmarshal(bytes, &message)
+	if err != nil {
+		log.Println("Unmarshal error")
+	}
+	fmt.Println(message.UserName)
+	fmt.Println(message.Content)
+	str.Connection.AddWSConn(conn, message.UserName, &message)
+	return str.Connection.OutChan
+}
+
+func SendJSON(conns []*websocket.Conn, str userConnections.Message) {
+	outcomingData, err := json.Marshal(&str)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, conn := range conns {
+		conn.WriteJSON(outcomingData)
+	}
+}
--- server/userConnections/connections.go
+++ server/userConnections/connections.go
@@ -1,21 +1,27 @@
 package userConnections
 
 import (
-	"github.com/gorilla/websocket"
 	"net"
+
+	"github.com/gorilla/websocket"
 )
 
-type Connections struct{
-	WSConnections map[*websocket.Conn]string // connection:login
-	TCPConnections map[net.Conn]string // connection:login
-	OutChan chan *Message
+type Connections struct {
+	WSConnections  map[*websocket.Conn]string // connection:login
+	TCPConnections map[net.Conn]string        // connection:login
+	OutChan        chan *Message
 }
 
-
-func (c *Connections) AddTCPConn(conn net.Conn, userName string, outChan *Message) *Connections{
+func (c *Connections) AddTCPConn(conn net.Conn, userName string, outChan *Message) *Connections {
 	str := c
 	str.TCPConnections[conn] = userName
 	str.OutChan <- outChan
 	return str
+}
 
+func (c *Connections) AddWSConn(conn *websocket.Conn, userName string, outChan *Message) *Connections {
+	str := c
+	str.WSConnections[conn] = userName
+	str.OutChan <- outChan
+	return str
 }

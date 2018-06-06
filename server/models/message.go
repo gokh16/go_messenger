0c9994b326121ed5ba0a74aee72b5dbc72be6b8f
KostyaF1 <k_moroz@hotmail.com>
2018-06-06 16:23:57 +0300

    modify Schema

--- server/models/message.go
+++ server/models/message.go
@@ -10,10 +10,9 @@ type Message struct {
 
 	User               User
 	Group              Group
-	MessageContentType MessageContentType
 
 	Content            string `json:"message_content"`
 	MessageSenderID    uint   `json:"message_sender_id"`
 	MessageRecipientID uint   `json:"message_recepient_id"`
-	//MessageContentTypeID uint   `json:"message_content_type_id"`
+	MessageContentType string `json:"message_content_type_id"`
 }

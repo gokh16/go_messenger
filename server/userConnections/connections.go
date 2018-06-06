43245d9186e95c1c812387c624eefeb9f13c664e
Aleksjj <komarov.oleksii@gmail.com>
2018-06-06 16:59:05 +0300

    Resolved confilcts

--- server/userConnections/connections.go
+++ server/userConnections/connections.go
@@ -2,14 +2,17 @@ package userConnections
 
 import (
 	"net"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type Connections struct {
-	WSConnections  map[*websocket.Conn]string // connection:login
-	TCPConnections map[net.Conn]string        // connection:login
-	OutChan        chan *Message
+	WSConnectionsMutex  *sync.Mutex
+	WSConnections       map[*websocket.Conn]string // connection:login
+	TCPConnectionsMutex *sync.Mutex
+	TCPConnections      map[net.Conn]string // connection:login
+	OutChan             chan *Message
 }
 
 func (c *Connections) AddTCPConn(conn net.Conn, userName string, outChan *Message) *Connections {
@@ -24,4 +27,16 @@ func (c *Connections) AddWSConn(conn *websocket.Conn, userName string, outChan *
 	str.WSConnections[conn] = userName
 	str.OutChan <- outChan
 	return str
+}
+
+func (c *Connections) GetAllTCPConnections() map[net.Conn]string {
+	c.TCPConnectionsMutex.Lock()
+	defer c.TCPConnectionsMutex.Unlock()
+	return c.TCPConnections
+}
+
+func (c *Connections) GetAllWSConnections() map[*websocket.Conn]string {
+	c.WSConnectionsMutex.Lock()
+	defer c.WSConnectionsMutex.Unlock()
+	return c.WSConnections
 }

5cf09698876965dbd2e2be16c590c6af93261787
Nikolay Khozhylin <nik.khozhylin@gmail.com>
2018-06-06 16:44:48 +0300

    Update message_db_service.go

--- server/db/dbservice/message_db_service.go
+++ server/db/dbservice/message_db_service.go
@@ -8,11 +8,7 @@ type Message struct {
 }
 
 //AddMessage func
-<<<<<<< HEAD
 func (msg Message) AddMessage(content, username, groupName, contentType string) bool {
-=======
-func (msg Message) AddMessage(content, username, groupName string, contentType string) bool {
->>>>>>> group-chat
 	sender := models.User{}
 	recipient := models.Group{}
 	dbConn.Where("username = ?", username).First(&sender)

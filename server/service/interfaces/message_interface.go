3c020cee0e2ac77c6c1aeaec3a6f929715d4c949
KostyaF1 <k_moroz@hotmail.com>
2018-06-06 17:07:09 +0300

    message interface

--- server/service/interfaces/message_interface.go
+++ server/service/interfaces/message_interface.go
@@ -2,11 +2,7 @@ package interfaces
 
 //messageInterface interface
 type messageInterface interface {
-<<<<<<< HEAD
 	AddMessage(content, username, groupName, contentType string) bool
-=======
-	AddMessage(content, username, groupName string, contentType string) bool
->>>>>>> group-chat
 }
 
 //MI is the type of messageInterface

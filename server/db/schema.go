14f98308957b184edf0dc5a790179314bbfb3de0
Niko <nik.khozhylin@gmail.com>
2018-06-06 16:51:26 +0300

    fix some files of db scheme

--- server/db/schema.go
+++ server/db/schema.go
@@ -33,15 +33,6 @@ func CreateDatabase() {
 		{
 			ID: "1",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&models.MessageContentType{}).Error
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.DropTable("message_content_types").Error
-			},
-		},
-		{
-			ID: "2",
-			Migrate: func(tx *gorm.DB) error {
 				return tx.AutoMigrate(&models.RelationType{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -49,7 +40,7 @@ func CreateDatabase() {
 			},
 		},
 		{
-			ID: "3",
+			ID: "2",
 			Migrate: func(tx *gorm.DB) error {
 				return tx.AutoMigrate(&models.User{}).Error
 			},
@@ -58,12 +49,11 @@ func CreateDatabase() {
 			},
 		},
 		{
-			ID: "4",
+			ID: "3",
 			Migrate: func(tx *gorm.DB) error {
 				db.CreateTable(&models.Message{})
 				db.Model(&models.Message{}).AddForeignKey("message_sender_id", "users(id)", "RESTRICT", "RESTRICT")
 				db.Model(&models.Message{}).AddForeignKey("message_recipient_id", "groups(id)", "RESTRICT", "RESTRICT")
-				db.Model(&models.Message{}).AddForeignKey("message_content_type_id", "message_content_types(id)", "RESTRICT", "RESTRICT")
 				return tx.AutoMigrate(&models.Message{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -71,7 +61,7 @@ func CreateDatabase() {
 			},
 		},
 		{
-			ID: "5",
+			ID: "4",
 			Migrate: func(tx *gorm.DB) error {
 				db.CreateTable(&models.Group{})
 				db.Model(&models.Group{}).AddForeignKey("group_owner_id", "users(id)", "RESTRICT", "RESTRICT")
@@ -83,7 +73,7 @@ func CreateDatabase() {
 			},
 		},
 		{
-			ID: "6",
+			ID: "5",
 			Migrate: func(tx *gorm.DB) error {
 				db.CreateTable(&models.GroupMember{})
 				db.Model(&models.GroupMember{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
@@ -96,7 +86,7 @@ func CreateDatabase() {
 			},
 		},
 		{
-			ID: "7",
+			ID: "6",
 			Migrate: func(tx *gorm.DB) error {
 				db.CreateTable(&models.UserRelation{})
 				db.Model(&models.UserRelation{}).AddForeignKey("relation_type_id", "relation_types(id)", "RESTRICT", "RESTRICT")

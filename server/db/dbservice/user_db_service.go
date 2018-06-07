43020078d3533a504a5bcac2c1c48d285f1b907e
KostyaF1 <k_moroz@hotmail.com>
2018-06-08 00:21:43 +0300

    adding some services in process...

--- server/db/dbservice/user_db_service.go
+++ server/db/dbservice/user_db_service.go
@@ -18,3 +18,23 @@ func (u User) CreateUser(user *models.User) bool {
 	}
 	return false
 }
+
+//LoginUser method get record from DB with using the gorm framework. It returns bool value.
+func (u User) LoginUser(user *models.User) bool {
+	dbConn.Where("login = ?", user.Login).Where("password = ?", user.Password).First(&user)
+	if dbConn.NewRecord(user) {
+		return false
+	}
+	return true
+}
+
+//GetUser method get record from DB with using the gorm framework. It returns User object.
+func (u User) GetUser(user *models.User) models.User {
+	dbConn.Where("login = ?", user.Login).First(&user)
+	return *user
+}
+
+func (u User) GetContactList() []models.User {
+	userList := []models.User{}
+	return userList
+}

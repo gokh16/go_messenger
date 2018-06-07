9f8a34f2bea61a051e1b01d0d676cb67c1684d0d
Misha Tkachenko <isfortis@gmail.com>
2018-06-07 19:25:57 +0300

    router out clearing channel

--- server/routerIn/routerIn.go
+++ server/routerIn/routerIn.go
@@ -2,28 +2,27 @@ package routerIn
 
 import (
 	"go_messenger/server/userConnections"
-	"log"
-	"go_messenger/server/service"
+	"fmt"
 )
 
 func RouterIn(c chan *userConnections.Message) {
 
 	// variable "action" is a command what to do with the structure
 	msg := <- c
-	action := msg.Action
-
-	switch action {
-
-	case "SendMessageTo":
-		go service.SendMessageTo(c)
-	case "CreateUser":
-		go service.CreateUser(c)
-	case "CreateGroup":
-		go service.CreateGroup(c)
-	case "AddGroupMember":
-		go service.AddGroupMember(c)
-
-	default:
-		log.Fatal("Unknown format of data")
-	}
+	fmt.Println(msg.Content)
+	//action := msg.Action
+	//switch action {
+	//
+	//case "SendMessageTo":
+	//	go service.SendMessageTo(c)
+	//case "CreateUser":
+	//	go service.CreateUser(c)
+	//case "CreateGroup":
+	//	go service.CreateGroup(c)
+	//case "AddGroupMember":
+	//	go service.AddGroupMember(c)
+	//
+	//default:
+	//	log.Fatal("Unknown format of data")
+	//}
 }
--- server/routerOut/routerOut.go
+++ server/routerOut/routerOut.go
@@ -2,41 +2,41 @@ package routerOut
 
 import (
 	"github.com/gorilla/websocket"
-	"go_messenger/server/handlers/tcp"
 	"go_messenger/server/userConnections"
 	"net"
-	"go_messenger/server/handlers/ws"
+	"fmt"
 )
 
 type RouterOut struct {
 	Connection *userConnections.Connections
-	ChOut      chan *userConnections.Message
 }
 
-func NewRouterOut(conn *userConnections.Connections, chOut chan *userConnections.Message) {
-	newRout := RouterOut{}
-	newRout.Connection = conn
-	newRout.ChOut = chOut
+func NewRouterOut(conn *userConnections.Connections) {
+	newRout := RouterOut{conn}
 	go newRout.HandleOut()
 }
 
 func (r *RouterOut) HandleOut() {
-	sliceTCP := r.getSliceOfTCP(r.ChOut)
-	sliceWS := r.getSliceOfWS(r.ChOut)
-
-	for msg := range r.ChOut {
-		if sliceTCP != nil {
-			tcp.WaitJSON(sliceTCP, msg)
-		}
-		if sliceWS != nil {
-			ws.SendJSON(sliceWS, msg)
-		}
+	for {
+		sliceTCP := r.getSliceOfTCP(r.Connection.OutChan)
+		sliceWS := r.getSliceOfWS(r.Connection.OutChan)
+		fmt.Println(sliceTCP)
+		fmt.Println(sliceWS)
+		//if sliceTCP != nil {
+		//	msg := <-r.Connection.OutChan
+		//	tcp.WaitJSON(sliceTCP, msg)
+		//}
+		//if sliceWS != nil {
+		//	msg := <-r.Connection.OutChan
+		//	ws.SendJSON(sliceWS, msg)
+		//}
 	}
 }
 
 func (r *RouterOut) getSliceOfTCP(c chan *userConnections.Message) []net.Conn {
 	mapTCP := r.Connection.GetAllTCPConnections()
 	var sliceTCP = make([]net.Conn, len(mapTCP))
+	fmt.Println(mapTCP)
 	for m := range c {
 		for k, _ := range mapTCP {
 			if mapTCP[k] == m.UserName {

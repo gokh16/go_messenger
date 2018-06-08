693360931efd22b1a5f5d6c502fd712b5aca3830
Niko <nik.khozhylin@gmail.com>
2018-06-08 15:51:39 +0300

    a lot of changes))

--- server/db/conf.go
+++ server/db/conf.go
@@ -3,8 +3,8 @@ package db
 const (
 	DB_HOST     = "0.0.0.0"
 	DB_PORT     = "5432"
-	DB_USER     = "postgres"
-	DB_PASSWORD = "postgres"
+	DB_USER     = "root"
+	DB_PASSWORD = "root"
 	DB_NAME     = "golangDB"
 	DB_SSLMODE  = "disable"
 )

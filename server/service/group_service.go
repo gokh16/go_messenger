333c4498d638d38f2a83c6b12b19fde1e142947e
KostyaF1 <k_moroz@hotmail.com>
2018-06-12 22:14:35 +0300

    fix service structure + add check is group exist

--- server/service/group_service.go
+++ server/service/group_service.go
@@ -1,33 +1,32 @@
 package service
 
 import (
+	"fmt"
 	"go_messenger/server/db/dbservice"
 	"go_messenger/server/service/interfaces"
 	"go_messenger/server/userConnections"
-	"fmt"
 )
 
-//CreateGroup function creats a special User and makes a record in DB. It returns bool value
+//CreateGroup function creats a special Group and makes a record in DB. It returns bool value
 func CreateGroup(message *userConnections.Message, chanOut chan *userConnections.Message) {
 	fmt.Println(message.GroupName)
-	var gmi interfaces.GMI = dbservice.GroupMember{}
 	var gi interfaces.GI = dbservice.Group{}
 	switch message.GroupType {
-	// groupType == 0 means privat message
+	// groupType == 1 means privat message
 	case 1:
 		ok := gi.CreateGroup(message.GroupName, message.GroupOwner, message.GroupType)
 		if ok {
 			for _, user := range message.GroupMember {
-				gmi.AddGroupMember(user, message.GroupName, "")
+				gi.AddGroupMember(user, message.GroupName, "")
 			}
 			message.Status = ok
 		}
 		message.Status = ok
-	// groupType == 1 means group chat
+	// groupType == 2 means group chat
 	case 2:
 		ok := gi.CreateGroup(message.GroupName, message.GroupOwner, message.GroupType)
 		if ok {
-			gmi.AddGroupMember(message.GroupOwner, message.GroupName, "")
+			gi.AddGroupMember(message.GroupOwner, message.GroupName, "")
 			message.Status = ok
 		}
 		message.Status = ok
@@ -37,12 +36,11 @@ func CreateGroup(message *userConnections.Message, chanOut chan *userConnections
 }
 
 func GetGroup(message *userConnections.Message, chanOut chan *userConnections.Message) {
-	var gmi interfaces.GMI = dbservice.GroupMember{}
-	var gi interfaces.GI = dbservice.Group{}
-	var mi interfaces.MI = dbservice.Message{}
-	group := gi.GetGroup(message.GroupName)
-	groupMessages := mi.GetGroupMessages(message.GroupName)
-	groupMembers := gmi.GetGroupUserList(message.GroupName)
+	// var gi interfaces.GI = dbservice.Group{}
+	// var mi interfaces.MI = dbservice.Message{}
+	// group := gi.GetGroup(message.GroupName)
+	// groupMessages := mi.GetGroupMessages(message.GroupName)
+	// groupMembers := gi.GetGroupUserList(message.GroupName)
 	chanOut <- message
 }
 
@@ -54,6 +52,22 @@ func EditGroup(message *userConnections.Message, chanOut chan *userConnections.M
 	chanOut <- message
 }
 
-func DeleteGroup() {
+//AddGroupMember add new members in spesific Group.
+func AddGroupMember(message *userConnections.Message, chanOut chan *userConnections.Message) {
+	var gi interfaces.GI = dbservice.Group{}
+	for _, user := range message.GroupMember {
+		gi.AddGroupMember(user, message.GroupName, message.LastMessage)
+	}
+}
+
+func GetGroupUserList() {
+
+}
+
+func DeleteGroupMember() {
+
+}
+
+func DeleteGroup(message *userConnections.Message, chanOut chan *userConnections.Message) {
 	chanOut <- message
 }
--- server/service/interfaces/group_interface.go
+++ server/service/interfaces/group_interface.go
@@ -9,6 +9,8 @@ type groupInterface interface {
 	CreateGroup(groupName, groupOwner string, groupType uint) bool
 	GetGroupList(userName string) []models.Group
 	GetGroup(groupName string) models.Group
+	AddGroupMember(username, groupName, lastmessage string) bool
+	GetGroupUserList(groupName string) []models.User
 	//Delete(group *models.Group)
 }
 

dbac0e1e7e765b9cd30d50d9a49cc9b189c36976
Mykhailo Tkachenko <32498400+justfortisz@users.noreply.github.com>
2018-06-17 18:58:19 +0300

    editing with gometalinter (#34)

--- server/db/dbservice/db_conn.go
+++ server/db/dbservice/db_conn.go
@@ -5,6 +5,7 @@ import (
 	"go_messenger/server/db"
 
 	"github.com/jinzhu/gorm"
+	//ignoring init from package below
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
@@ -13,7 +14,7 @@ var dbConn *gorm.DB
 // OpenConnDB opens a connection eith DB it returns a *DB object for closing the connection in main
 func OpenConnDB() *gorm.DB {
 	var err error
-	dbinfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", db.DB_HOST, db.DB_PORT, db.DB_USER, db.DB_NAME, db.DB_PASSWORD, db.DB_SSLMODE)
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", db.HostDB, db.PortDB, db.UserDB, db.NameDB, db.PasswordDB, db.SSLModeDB)
 	dbConn, err = gorm.Open("postgres", dbinfo)
 	if err != nil {
 		fmt.Println("gorm Open connection error: ", err)
--- server/main.go
+++ server/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"sync"
-
 	"go_messenger/server/db"
 	"go_messenger/server/db/dbservice"
 	"go_messenger/server/handlers/tcp"
 	"go_messenger/server/handlers/ws"
 	"go_messenger/server/routerOut"
 	"go_messenger/server/userConnections"
+	"log"
+	"net"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -23,28 +23,34 @@ func init() {
 
 func main() {
 	chOut := make(chan *userConnections.Message, 1024)
-
 	connectionList := userConnections.Connections{}
-
 	connectionList.OutChan = chOut
 	connectionList.WSConnectionsMutex = new(sync.Mutex)
-	connectionList.WSConnections = make(map[*websocket.Conn]string, 0)
+	connectionList.WSConnections = make(map[*websocket.Conn]string)
 	connectionList.TCPConnectionsMutex = new(sync.Mutex)
-	connectionList.TCPConnections = make(map[net.Conn]string, 0)
+	connectionList.TCPConnections = make(map[net.Conn]string)
 
 	routerOut.NewRouterOut(&connectionList)
 
-	ws.NewWSHandler(&connectionList)
+	ws.NewHandlerWS(&connectionList)
 	fmt.Println("good")
 
-	tcp.NewTCPHandler(&connectionList)
+	tcp.NewHandlerTCP(&connectionList)
 	fmt.Println("good2")
 
 	db := dbservice.OpenConnDB()
-	defer db.Close()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	fmt.Println("good3")
 
 	stop := make(chan bool)
 	<-stop
 }
+
+//TODO GOLINTERS
+//TODO waitgroups

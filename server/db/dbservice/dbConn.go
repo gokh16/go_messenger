9d61b5ebe63d2651152e4a6e4d5a4d406e28cbdc
KostyaF1 <k_moroz@hotmail.com>
2018-06-20 02:52:22 +0300

    changed files

---
+++ server/db/dbservice/dbConn.go
@@ -0,0 +1,24 @@
+package dbservice
+
+import (
+	"fmt"
+	"go_messenger/server/db"
+
+	"github.com/jinzhu/gorm"
+	//ignoring init from package below
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var dbConn *gorm.DB
+
+// OpenConnDB opens a connection eith DB it returns a *DB object for closing the connection in main
+func OpenConnDB() *gorm.DB {
+	var err error
+	dbinfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", db.HostDB, db.PortDB, db.UserDB, db.NameDB, db.PasswordDB, db.SSLModeDB)
+	dbConn, err = gorm.Open("postgres", dbinfo)
+	if err != nil {
+		fmt.Println("gorm Open connection error: ", err)
+	}
+	fmt.Println("db open ok")
+	return dbConn
+}

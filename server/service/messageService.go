dcccf806cf0e7c5b3ab8ac6e9f50cd6124b97b2f
Niko <nik.khozhylin@gmail.com>
2018-06-19 18:01:12 +0300

    some changes of structs messageIn and messageOut

--- server/service/messageService.go
+++ server/service/messageService.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"go_messenger/server/db/dbservice"
-	"go_messenger/server/models"
 	"go_messenger/server/service/serviceModels"
 	"go_messenger/server/userConnections"
 )
@@ -13,12 +12,12 @@ type MessageService struct {
 }
 
 func (m *MessageService) SendMessageTo(msg *userConnections.MessageIn, chanOut chan *serviceModels.MessageOut) {
-	m.MessageDBService.AddMessage(msg.Message.Content, msg.User.Username, msg.Group.GroupName, msg.Message.MessageContentType)
-	var groupMembers []models.User
-	userList := m.GroupDBService.GetGroupUserList(msg.Group.GroupName)
-	for _, user := range userList {
-		groupMembers = append(msg.Member.GroupMembers, user)
-	}
-	msg.Member.GroupMembers = groupMembers
-	chanOut <- msg
+	//m.MessageDBService.AddMessage(msg.Message.Content, msg.User.Username, msg.Group.GroupName, msg.Message.MessageContentType)
+	//var groupMembers []models.User
+	//userList := m.GroupDBService.GetGroupUserList(msg.Group.GroupName)
+	//for _, user := range userList {
+	//	groupMembers = append(msg.Member.GroupMembers, user)
+	//}
+	//msg.Member.GroupMembers = groupMembers
+	//chanOut <- msg
 }

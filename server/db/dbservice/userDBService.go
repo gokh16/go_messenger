b3158d1b5e22113eec34ecf4bbe4d62d47617f7f
Niko <nik.khozhylin@gmail.com>
2018-06-27 23:14:21 +0300

    created init for services

--- server/db/dbservice/userDBService.go
+++ server/db/dbservice/userDBService.go
@@ -11,7 +11,7 @@ type UserDBService struct {
 
 //CreateUser method creates User in DB.
 //It returns bool value.
-func (u UserDBService) CreateUser(user *models.User) bool {
+func (u *UserDBService) CreateUser(user *models.User) bool {
 	dbConn.Where("username = ?", user.Username).First(&user)
 	if dbConn.NewRecord(user) {
 		dbConn.Create(&user)
@@ -21,7 +21,7 @@ func (u UserDBService) CreateUser(user *models.User) bool {
 }
 
 //LoginUser - user's auth.
-func (u UserDBService) LoginUser(user *models.User) bool {
+func (u *UserDBService) LoginUser(user *models.User) bool {
 	dbConn.Where("login = ?", user.Login).Where("password = ?", user.Password).Take(&user)
 	if dbConn != nil {
 		return true
@@ -30,7 +30,7 @@ func (u UserDBService) LoginUser(user *models.User) bool {
 }
 
 //AddContact add spesial user to contact list of special User
-func (u UserDBService) AddContact(user, contact *models.User, relationType uint) bool {
+func (u *UserDBService) AddContact(user, contact *models.User, relationType uint) bool {
 	dbConn.Where("username = ?", user.Username).First(&user)
 	dbConn.Where("username = ?", contact.Username).First(&contact)
 	relation := models.UserRelation{RelatingUser: user.ID, RelatedUser: contact.ID, RelationTypeID: relationType}
@@ -43,20 +43,20 @@ func (u UserDBService) AddContact(user, contact *models.User, relationType uint)
 }
 
 //GetUsers method gets all users from DB.
-func (u UserDBService) GetUsers(users *[]models.User) {
+func (u *UserDBService) GetUsers(users *[]models.User) {
 	dbConn.Find(&users)
 }
 
 //GetUser method get special user from DB.
 //It returns User object.
-func (u UserDBService) GetUser(user *models.User) *models.User {
+func (u *UserDBService) GetUser(user *models.User) *models.User {
 	dbConn.Where("login = ?", user.Login).Take(&user)
 	return user
 }
 
 //GetContactList gets contact list of special user from DB.
 //It returns slice []models.User.
-func (u UserDBService) GetContactList(user *models.User) []models.User {
+func (u *UserDBService) GetContactList(user *models.User) []models.User {
 	contactList := []models.User{}
 	temp := []models.UserRelation{}
 	dbConn.Where("login = ?", user.Login).First(&user)

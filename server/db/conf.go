9c36b82aa3580c1dffad640d5dbeb6fce33ec842
Giperboloid <kostyamol@gmail.com>
2018-06-27 17:42:43 +0300

    NewVer 27

--- server/db/conf.go
+++ server/db/conf.go
@@ -3,7 +3,7 @@ package db
 //block with configuration constants for connecting to database
 const (
 	HostDB     = "0.0.0.0"
-	PortDB     = "5434"
+	PortDB     = "5432"
 	UserDB     = "postgres" // postgres
 	PasswordDB = "golang"    // admin
 	NameDB     = "golangDB"
--- server/models/groupType.go~
+++ server/models/groupType.go~
@@ -8,5 +8,5 @@ import (
 type GroupType struct {
 	gorm.Model
 
-	Type string
+	Type string `json:"group_type"`
 }

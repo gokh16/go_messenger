00f1af5ce475a4b205faacff192eaaa9e450e139
Misha Tkachenko <isfortis@gmail.com>
2018-06-28 18:51:04 +0300

    error window and getting messages

--- server/handlers/tcp/tcpHandlerOut.go
+++ server/handlers/tcp/tcpHandlerOut.go
@@ -10,7 +10,7 @@ import (
 
 //WaitJSON is waiting for data from route out, parsing data into json format and write to util
 func WaitJSON(conns []net.Conn, str *serviceModels.MessageOut) {
-	log.Println("handler out", str.Message.Content, str.User.ID)
+	//log.Println("handler out", str.GroupList[0].Messages[0].Content, str.User.ID)
 
 	//for _, as := range str.GroupList{
 	//	fmt.Println(as.GroupName)

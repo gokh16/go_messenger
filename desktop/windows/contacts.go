ca3885fe9a7dd6126600d1a56f18b8fffdff1d19
Misha Tkachenko <isfortis@gmail.com>
2018-07-02 16:16:14 +0300

    get contacts

--- desktop/windows/contacts.go
+++ desktop/windows/contacts.go
@@ -7,6 +7,7 @@ import (
 	"go_messenger/desktop/config"
 	"go_messenger/desktop/util"
 	"log"
+	"time"
 )
 
 func DrawContactsWindow(conn net.Conn) {
@@ -15,14 +16,15 @@ func DrawContactsWindow(conn net.Conn) {
 	users := make([]structure.User, 0)
 	go func() {
 		for {
-			log.Println("contacts")
 			msg := util.JSONdecode(conn)
 			users = msg.ContactList
-			log.Println(msg.Action, users[0].Login, "contacts")
+			log.Println(users)
 		}
 	}()
+	time.Sleep(1*time.Second)
 	buttonUserSlice := make([]*ui.Button, 0)
 	for _, user := range users {
+		log.Println(config.Login, user.Login)
 		if user.Login != config.Login {
 			buttonWithUser := ui.NewButton(user.Login)
 			usersBox.Append(buttonWithUser, false)

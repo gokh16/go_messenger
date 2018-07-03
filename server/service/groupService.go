07caf72d240ee3d5eef0f7d65444695b243226ce
Misha Tkachenko <isfortis@gmail.com>
2018-07-03 16:22:45 +0300

    group service

--- server/service/groupService.go
+++ server/service/groupService.go
@@ -1,10 +1,10 @@
 package service
 
 import (
+	"go_messenger/server/service/interfaces"
 	"go_messenger/server/service/serviceModels"
 	"go_messenger/server/userConnections"
 	"log"
-	"go_messenger/server/service/interfaces"
 )
 
 //GroupService ...
@@ -26,27 +26,15 @@ func (g *GroupService) CreateGroup(messageIn *userConnections.MessageIn, chanOut
 	ok := g.groupManager.CreateGroup(&messageIn.Group)
 	if ok {
 		switch messageIn.Group.GroupTypeID {
-
 		// groupTypeID == 1 means privat message
 		case 1:
-			g.messageManager.AddMessage(&messageIn.Message)
 			for _, member := range messageIn.Members {
 				g.groupManager.AddGroupMember(&member, &messageIn.Group, &messageIn.Message)
 			}
-
-			groupOut := serviceModels.Group{
-				GroupName: messageIn.Group.GroupName,
-				GroupType: messageIn.Group.GroupType,
-				Members:   messageIn.Members,
-				Messages:  g.messageManager.GetGroupMessages(&messageIn.Group, messageIn.MessageLimit),
-			}
-
-			messageOut.GroupList = append(messageOut.GroupList, groupOut)
-			messageOut.Recipients = messageIn.Members
-
 			// groupType == 2 means group chat
 		case 2:
 			g.groupManager.AddGroupMember(&messageIn.Group.User, &messageIn.Group, &messageIn.Message)
+
 		}
 	}
 	messageOut.Status = ok
@@ -57,6 +45,7 @@ func (g *GroupService) CreateGroup(messageIn *userConnections.MessageIn, chanOut
 func (g *GroupService) GetGroup(messageIn *userConnections.MessageIn, chanOut chan<- *serviceModels.MessageOut) {
 	messageOut := serviceModels.MessageOut{User: messageIn.User, Action: messageIn.Action}
 	groupModel := g.groupManager.GetGroup(&messageIn.Group)
+	log.Printf("GET GROUP SERVICE, group_id -> %d, group_name -> %s", groupModel.ID, groupModel.GroupName)
 	members := g.groupManager.GetMemberList(&groupModel)
 	messages := g.messageManager.GetGroupMessages(&groupModel, messageIn.MessageLimit)
 	for i, msg := range messages {
@@ -71,8 +60,8 @@ func (g *GroupService) GetGroup(messageIn *userConnections.MessageIn, chanOut ch
 func (g *GroupService) GetGroupList(messageIn *userConnections.MessageIn, chanOut chan<- *serviceModels.MessageOut) {
 	messageOut := serviceModels.MessageOut{User: messageIn.User, Action: messageIn.Action}
 	groupModelList := g.groupManager.GetGroupList(&messageIn.User)
-	log.Println(groupModelList)
 	for _, gr := range groupModelList {
+		log.Printf("GET GROUP LIST SERVICE, group_id -> %d, group_name -> %s", gr.ID, gr.GroupName)
 		members := g.groupManager.GetMemberList(&gr)
 		messages := g.messageManager.GetGroupMessages(&gr, messageIn.MessageLimit)
 		groupOut := serviceModels.NewGroup(gr, members, messages)
